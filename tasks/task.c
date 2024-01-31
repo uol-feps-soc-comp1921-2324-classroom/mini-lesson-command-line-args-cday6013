94c6bd28b0a9df0271c145042e91fc9055afcc28
cday6013 <151550138+cday6013@users.noreply.github.com>
2024-01-31 13:34:00 +0000

    Added cmd line arguments and basic validation

--- tasks/task.c
+++ tasks/task.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int num1, num2;
+int main(int argc, char *argv[]) {
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    //check there are 2 cmd line inputs
+    if (argc != 3){
+        printf("Expected usage: ./sum <int1> <int2>\n");
+        return 1; //exit unsuccessfully
+    } 
+    
+    //check both inputs are ints
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
-
+    if (num1 == 0 || num2 == 0){ //atoi returns 0 if integer conversion is unsuccessful. Not useful if the person wants to add zero though
+        printf("Expected usage: ./sum <int1> <int2>\n");
+        return 1; //exit unsuccessfully
+    }
+    //NOTE: this does not check for floats, just truncates them
+    
     // Calculate and print the sum
     int sum = num1 + num2;
     printf("Sum: %d\n", sum);
